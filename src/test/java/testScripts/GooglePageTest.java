c4d9d08009cbf29049a5f6d47525d5ad50fa4631
ShaliniLaura <emailid>
2024-04-18 07:31:37 +0000

    Java Tutorial Search

--- src/test/java/testScripts/GooglePageTest.java
+++ src/test/java/testScripts/GooglePageTest.java
@@ -1,15 +1,28 @@
 package testScripts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GooglePageTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Selenium Manager
+		//ChromeOptions options= new ChromeOptions();
+		//options.setBrowserVersion("115");
+		//WebDriver driver=new ChromeDriver(options);
+		
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
+		System.out.println("Page Title: "+ driver.getTitle());
+		WebElement srcBox=driver.findElement(By.id("APjFqb"));
+		srcBox.sendKeys("Java Tutorial");
+		srcBox.sendKeys(Keys.ENTER);
 		System.out.println("Page Title: "+ driver.getTitle());
 
 	}
